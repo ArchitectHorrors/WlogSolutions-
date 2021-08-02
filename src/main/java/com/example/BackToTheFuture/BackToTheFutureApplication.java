e19a145b05c5306d0286e772afa85c79440718bd
Szymon Medrala <disasterarchitectvr@gmail.com>
2021-08-02 09:48:06 +0200

    Add first project

---
+++ src/main/java/com/example/BackToTheFuture/BackToTheFutureApplication.java
@@ -0,0 +1,16 @@
+package com.example.BackToTheFuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToTheFutureApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BackToTheFutureApplication.class, args);
+	}
+
+}
+
+
